111d570a86ec308128f34bfadef47f33bae6718e
MonaLinuxWorld <78268545+MonaLinuxWorld@users.noreply.github.com>
2021-02-26 23:48:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "LW <a href=''>Hello, World</a>!!!!!"
+    return "LW <a href=''>Hello, World</a>!!!!! <img src='https://sdfdgfhghy.blob.core.windows.net/mystttt/vimal_photo.jpg' />"
